0092fb043f5434fdadb8fc94bc11024ae7653dce
tomprochy <prochazka.tomas82@gmail.com>
2019-06-26 22:40:06 +0200

    cmt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AckoComponent } from './acko/acko.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AckoComponent
   ],
   imports: [
     BrowserModule
